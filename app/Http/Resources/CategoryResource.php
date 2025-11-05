f113ada099adbca5cc914e65830532f9c4213974
yosraldeenmakdh-pixel <yosraldeenmakdh@gmail.com>
2025-11-05 20:58:04 +0300

    some changes

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -13,7 +13,7 @@ class CategoryResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'description' => $this->description,
-            'image' => $this->image ? asset('public/uploads' . $this->image) : null,
+            'image' => $this->image ? asset('public/uploads/' . $this->image) : null,
             'meals_count' => $this->meals_count,
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
