9ec3131b83306c9d29ac5ae9ab2920df3f20590b
yosraldeenmakdh-pixel <yosraldeenmakdh@gmail.com>
2026-02-14 18:14:34 +0300

    project commit

--- app/Filament/Resources/OnlineOrderConfirmations/Tables/OnlineOrderConfirmationsTable.php
+++ app/Filament/Resources/OnlineOrderConfirmations/Tables/OnlineOrderConfirmationsTable.php
@@ -36,7 +36,7 @@ class OnlineOrderConfirmationsTable
                 TextColumn::make('order_number')
                     ->label('رقم الطلب')
                     ->searchable()
-                    ->description(fn($record) => $record->order->confirmed_at->diffForHumans())
+                    ->description(fn($record) => $record->confirmed_at->diffForHumans())
                     ->sortable()
                     ->weight('bold')
                     ->color('primary'),
